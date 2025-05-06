6300e1745630b1e46d17e9a7adc365dde63b7b36
Roli Jule <22965747+RoliJule@users.noreply.github.com>
2025-05-05 23:52:53 -0400

    updated the styling and added intro to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import styles from '../styles/homeStyles.css';
 import RJLogo from '../components/icons/RJLogo';
 import Message  from '../components/Message';
-import portrait from '../assets/Portrait/portrait-picture-v1.webp';
+import portrait from '../assets/Portrait/portrait-cutout.png';
 import herobg from '../assets/Portrait/ChatGPT Image Apr 29, 2025, 11_26_12 AM.png';
 function Home() {
     return (
@@ -9,15 +9,24 @@ function Home() {
             <div className="screen-wrapper">
                 <div className="screen-bezel">
                     <div className="screen-content">
+                        <div id="introduction-wrapper">
+                            <h1 className='intro-header'>Hi,</h1>
+                            <span className='intro-header'>I'm </span><Message className="intro-header intro-message" text={`Rolando Jule.`} speed={100}/>
+                            <p className='intro-about'>I'm a Full Stack Web Developer who builds clean, scalable, and user-focused web solutions. I enjoy working across both front-end and back-end to turn ideas into functional, impactful products.</p>
+                        </div>
                         <RJLogo className={'screen-icon'}/>
-                        <Message className="intro-message" text={`->Hello, \n->My name is Rolando Jule. \n->Welcome and enjoy!`} speed={100}/>
+                        <div id="profile-wrapper">
+                            <img id="profile-img" src={portrait} />
+                        </div>
                     </div>
                 </div>
-                <div id="homepage-nav">
-                <a href="">About</a>
-                <a href="">Explore</a>
-                <a href="">Projects</a>
+                <div class="stand-neck"></div>
             </div>
+            <div id="homepage-nav">
+                    <a href="">Home</a>
+                    <a href="">About</a>
+                    <a href="">Explore</a>
+                    <a href="">Projects</a>
             </div>
         </>
     );
